9c07dcfc377cbb5b5b85c90a455c6fb85ae97898
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2023-11-28 18:02:40 +0900

    fix deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
-docker exec -it app php artisan migrate
-docker exec -it app php artisan config:clear
-docker exec -it app php artisan cache:clear
-docker exec -it app php artisan view:clear
-docker exec -it app php artisan route:clear
+docker exec -it app1 php artisan migrate
+docker exec -it app1 php artisan config:clear
+docker exec -it app1 php artisan cache:clear
+docker exec -it app1 php artisan view:clear
+docker exec -it app1 php artisan route:clear
