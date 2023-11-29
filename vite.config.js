aa101a8f18705ba5a031583b050471932bc291fe
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2023-11-29 12:55:50 +0900

    update ports

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
+        port: 5174,
         host: '0.0.0.0',
+        strictPort: true,
         hmr: {
             host: 'localhost',
         },
